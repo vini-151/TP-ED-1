59cc7db147cf6f831c98cc97e06dbcedd035ff44
Vini <ovini.15515@gmail.com>
2024-04-20 11:22:02 -0300

    Update trabalhoGrup1.c

--- EstruturaDados/trabalhoGrup1.c
+++ EstruturaDados/trabalhoGrup1.c
@@ -6,7 +6,7 @@
 #define CELULAR_TAM 15
 #define CURSO_TAM 30
 
-struct aluno {
+struct Aluno {
     char nome[NOME_TAM];
     char telefone[CELULAR_TAM];
     char curso[CURSO_TAM];
@@ -16,13 +16,15 @@ struct aluno {
 
 const char* passouMedia(float media);
 
+void ValidaMedia(struct Aluno *a, float media);
+
 int main(void) {
     FILE *entrada, *saida;
     entrada = fopen("DadosEntrada.csv", "r");
     saida = fopen("DadosSaida.csv", "w");
 
     if (entrada != NULL && saida != NULL) {
-        struct aluno x;
+        struct Aluno x;
         char line[256]; 
         while (fgets(line, sizeof(line), entrada)) {
             sscanf(line, "%59[^,],%14[^,],%29[^,],%f,%f", x.nome, x.telefone, x.curso, &x.nota1, &x.nota2);
@@ -30,10 +32,7 @@ int main(void) {
                 fprintf(saida, "Nome,media,situacao\n");
             }else{
                 float valorDaMedia = (x.nota1 + x.nota2) / 2;
-                if (valorDaMedia < 0 || valorDaMedia > 10) {
-                    printf("Erro: média inválida para o aluno %s\n", x.nome);
-                    continue; 
-                }
+                ValidaMedia(&x, valorDaMedia);
                 const char *situacao = passouMedia(valorDaMedia);
                 fprintf(saida, "%s,%.2f,%s\n", x.nome, valorDaMedia, situacao);
             }
@@ -48,6 +47,13 @@ int main(void) {
 
     return 0;
 }
+
+void ValidaMedia(struct Aluno *a, float media){
+    if (media < 0 || media > 10) {
+        printf("Erro: média inválida para o Aluno %s\n", a->nome);
+    }
+}
+
 
 const char* passouMedia(float media) {
     return (media >= 7.0) ? "APROVADO" : "REPROVADO";
